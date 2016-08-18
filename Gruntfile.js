dd2c9b557af89da845e0f5df9fe20b02603565e4
lrdzero <elendil.capt.gondor@gmail.com>
2016-08-18 10:23:33 -0700

    Grunfile mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,17 @@ module.exports = function(grunt) {
 	  }
 	  }
   }
+  mochaTest: {
+	test: {
+	   options:{
+		reporter: 'spec',
+		captureFile: 'docs/test/results.txt',
+		quiet: false,
+		clearRequireCache:false
+	    },
+	    src: ['test/**/*.js']
+	}
+  }
   });
 
   // Carga el plugin de grunt para hacer esto
@@ -20,6 +31,7 @@ module.exports = function(grunt) {
 
   // Tarea por omisión: generar la documentación
   grunt.registerTask('default', ['docco']);
+  grunt.registerTask('default', 'mochaTest');
 };
 
 
